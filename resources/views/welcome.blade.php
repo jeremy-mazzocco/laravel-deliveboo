99bf829cd5da3d89ee90ca4a364b6c795c8a9c95
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-09-04 18:12:16 +0200

    test branch and marge Nicola

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,9 +23,7 @@
 
     <div class="content">
         <div class="container">
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi
-                deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis
-                accusamus dolores!</p>
+            <p>Hello World!</p>
         </div>
     </div>
 @endsection
