cab58bb165562ff2d1dfc1cda76bf1480e9a1b48
gabrielemafrica <gabriele.ga@live.it>
2023-09-06 14:04:37 +0300

    show-dishes end

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Dish;
 use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
@@ -12,5 +15,11 @@ class DashboardController extends Controller
     {
 
         return view('dashboard.section.home-user');
+    }
+    public function show()
+    {
+        $userId = Auth::user()->id;
+        $dishes = Dish::where('user_id', $userId)->get();
+        return view('dashboard.section.dish-show', compact('dishes'));
     }
 }
