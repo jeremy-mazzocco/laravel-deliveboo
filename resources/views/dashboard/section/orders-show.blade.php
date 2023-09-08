1f2039108e8846cf9fec03a13288c74aa979d7e5
Jeremy Mazzocco <68953007+jeremy-mazzocco@users.noreply.github.com>
2023-09-08 12:05:55 +0200

    push

--- resources/views/dashboard/section/orders-show.blade.php
+++ resources/views/dashboard/section/orders-show.blade.php
@@ -9,26 +9,34 @@
         @foreach ($orders as $order)
             <div class="order w-50 border-2 border border-secondary m-auto my-2 p-2">
                 <h3>Ordine ID: {{ $order->id }}</h3>
-                <div class="customer-info">
-                    Nome cliente: {{ $order->customer_name }}
-                    Indirizzo consegna: {{ $order->customer_address }}
-                    Numero di telefono: {{ $order->phone_number }}
-                    Email: {{ $order->email }}
+                <div class="customer-info my-2">
+                    <div>
+                        Nome cliente: {{ $order->customer_name }}
+                    </div>
+                    <div>
+                        Indirizzo consegna: {{ $order->customer_address }}
+                    </div>
+                    <div>
+                        Numero di telefono: {{ $order->phone_number }}
+                    </div>
+                    <div>
+                        Email: {{ $order->email }}
+                    </div>
                 </div>
                 <div class="order-details">
                     @foreach ($order->dishes as $dish)
                         <div class="dish">
-                            Nome pietanza: {{ $dish->dish_name }}
-                            Quantità: {{ $dish->pivot->amount }}
-                            Prezzo unitario: &euro; {{ $dish->price }}
-                            Ora ordinazione: {{ $order->time }}
-                            Totale per questo piatto: &euro; {{ $dish->price * $dish->pivot->amount }}
-                            <div
-                                class="@if ($order->status) bg-success @else bg-danger @endif text-light p-1 w-25 text-center rounded">
-                                <b>{{ $order->status ? 'Pagato' : 'Da pagare' }}</b>
-                            </div>
+                            <div>Nome pietanza: {{ $dish->dish_name }}</div>
+                            <div>Quantità: {{ $dish->pivot->amount }}</div>
+                            <div>Prezzo unitario: &euro; {{ $dish->price }}</div>
+                            <div>Ora ordinazione: {{ $order->time }}</div>
+                            <div>Totale per questo piatto: &euro; {{ $dish->price * $dish->pivot->amount }}</div>
                         </div>
                     @endforeach
+                    <div
+                        class="@if ($order->status) bg-success @else bg-danger @endif text-light p-1 w-25 text-center rounded">
+                        <b>{{ $order->status ? 'Pagato' : 'Da pagare' }}</b>
+                    </div>
                 </div>
 
             </div>
