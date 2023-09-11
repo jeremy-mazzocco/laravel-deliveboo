b5a38ed26351372aba63dc78694de9c02fa5c93d
Jeremy Mazzocco <68953007+jeremy-mazzocco@users.noreply.github.com>
2023-09-11 12:48:54 +0200

    push

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Type;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function typeList()
+    {
+        $types = Type::all();
+
+        return response()->json([
+            'types' => 'ciaoo'
+        ]);
+    }
+}
